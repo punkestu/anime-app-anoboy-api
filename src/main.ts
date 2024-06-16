4fbcf1df2f8498ee20450cd4d8b2b6cf17f14837
punkestu <pangestubima89@gmail.com>
2024-06-17 01:16:57 +0700

    turn off simple cors

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,15 @@ import { NextFunction, Request, Response } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(function (request: Request, response: Response, next: NextFunction) {
-    if (
-      process.env.NODE_ENV === 'production' &&
-      request.hostname !== process.env.CORS_ORIGIN
-    ) {
-      return response.status(401).json({
-        status: 401,
-        message: 'You are not allowed to access this resource',
-      });
-    }
+    // if (
+    //   process.env.NODE_ENV === 'production' &&
+    //   request.hostname !== process.env.CORS_ORIGIN
+    // ) {
+    //   return response.status(401).json({
+    //     status: 401,
+    //     message: 'You are not allowed to access this resource',
+    //   });
+    // }
     next();
   });
   await app.listen(3000);
